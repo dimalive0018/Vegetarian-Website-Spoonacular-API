b96b325558be2578abd63fb7ddf4cc627b3ab482
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-04-14 00:28:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/main.js',
-    },
-  },
   envDir: '.',
   define: {
     'process.env': loadEnv(process.env.NODE_ENV, process.cwd()),
