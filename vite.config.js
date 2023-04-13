92793a4db9fe3f25608b4182fb7fedf60994c80e
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-04-14 01:38:59 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: { 'process.env.NODE_ENV': '"production"' }
+  define: { 'process.env.VITE_API_KEY': '"production"' }
 })
