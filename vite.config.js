04992db44ff5e79208d85ba29f42498bca1d2d97
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-04-14 01:26:00 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: '.',
-  define: {
-    'process.env': loadEnv(process.env.NODE_ENV, process.cwd()),
-  },
-});
+})
